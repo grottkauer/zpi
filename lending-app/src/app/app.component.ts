ac307b6350f1653b928dbd7766fce860413a433c
Ola Druciak <ola.druciak@wp.pl>
2019-03-13 22:30:19 +0100

    Added fields to .ts file, almost done google and facebook logging, check/create client id.

--- lending-app/src/app/app.component.ts
+++ lending-app/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService, SocialUser} from 'angularx-social-login';
+import {FacebookLoginProvider, GoogleLoginProvider} from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'lending-app';
+  username: string;
+  password: string;
+  showSpinner: boolean;
+
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(private authService: AuthService) {  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  login() {
+    // place for login method
+  }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = user != null;
+    });
+  }
 }
--- lending-app/src/app/app.module.ts
+++ lending-app/src/app/app.module.ts
@@ -4,9 +4,36 @@ import { NgModule } from '@angular/core';
 import {CustomMaterialModule} from './core/material.module';
 import {FormsModule } from '@angular/forms';
 import { FlexLayoutModule} from '@angular/flex-layout';
+import {SocialLoginModule, AuthServiceConfig, LoginOpt} from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider} from 'angularx-social-login';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+
+const fbLoginOptions: LoginOpt = {
+  scope: 'pages_messaging,pages_messaging_subscriptions,email,pages_show_list,manage_pages',
+  return_scopes: true,
+  enable_profile_selector: true
+}; // https://developers.facebook.com/docs/reference/javascript/FB.login/v2.11
+
+const googleLoginOptions: LoginOpt = {
+  scope: 'profile email'
+}; // https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauth2clientconfig
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('Google-OAuth-Client-ID')
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('Facebook-App-Id')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -18,9 +45,15 @@ import { UserComponent } from './user/user.component';
     BrowserAnimationsModule,
     CustomMaterialModule,
     FormsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
