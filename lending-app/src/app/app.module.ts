4646dead04e51fc02f2dff140277e6be80de8a87
Natalia <natalia-gasiorowska@o2.pl>
2019-03-14 09:38:33 +0100

    Revert "created proper components"
    This reverts commit b9edf5e770cc7ac72a16c3fc2a1e8f0c7fa57802.

--- lending-app/src/app/app.module.ts
+++ lending-app/src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { NgModule } from '@angular/core';
 import {CustomMaterialModule} from './core/material.module';
 import {FormsModule } from '@angular/forms';
 import { FlexLayoutModule} from '@angular/flex-layout';
 
-import { AppRoutingModule } from './core/app.routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    LoginComponent
+    UserComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CustomMaterialModule,
     FormsModule,
-    FlexLayoutModule,
-    AppRoutingModule
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
