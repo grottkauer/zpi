8c805eb75c0630193bc4d255aae0286c03fe765e
Natalia <natalia-gasiorowska@o2.pl>
2019-03-14 01:46:22 +0100

    added prototype facebook login

--- lending-app/src/app/login/login.component.ts
+++ lending-app/src/app/login/login.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private router: Router) {
   }
 
   login() {
+    this.router.navigateByUrl('/user');
+  }
+
+  myFacebookLogin() {
+    FB.login(response => {
+      if (response.authResponse) {
+          this.router.navigateByUrl('/user');
+      } else {
+
+        console.log('Nie zalogowano');
+      }
+    }, {scope: 'user_location'});
 
   }
+
 }
