027528a0a6bad9f8d22b23bf26ff4e8a2f808f63
Ola Druciak <ola.druciak@wp.pl>
2019-03-25 22:54:12 +0100

    Dodanie struktury projektu spring bootowego - propozycja wstepna

---
+++ lending/src/main/java/com/lending/LendingAppApplication.java
@@ -0,0 +1,13 @@
+package com.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingAppApplication.class, args);
+	}
+
+}
---
+++ lending/src/main/java/com/lending/controller/ContactController.java
@@ -0,0 +1,12 @@
+package com.lending.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+    /*@RequestMapping(value="/kontakt")
+    public String contact(){
+        return "kontakt";
+    }*/
+}
