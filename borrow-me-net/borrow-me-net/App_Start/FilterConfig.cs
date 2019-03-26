4a2c1e514f1b29b7f8dbd0cc2332395ca9c758ce
Natalia <natalia-gasiorowska@o2.pl>
2019-03-26 23:11:27 +0100

    .net framework project

---
+++ borrow-me-net/borrow-me-net/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace borrow_me_net
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
