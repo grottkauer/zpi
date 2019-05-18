aa34109f9b0b41f7fcf218c921dd6382d591a84d
Ola Druciak <ola.druciak@wp.pl>
2019-05-18 18:56:18 +0200

    Added default location for Wroclaw

--- src/main/resources/static/leaflet/map.js
+++ src/main/resources/static/leaflet/map.js
@@ -2,7 +2,7 @@ var mymap;
 var feature;
 
 function load_map() {
-	mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	mymap = L.map('mapid').setView([51.1000000, 17.0333300], 13);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
