0ec11f9497a560219e32ac3ea3afaa1447c5e768
Natalia <natalia-gasiorowska@o2.pl>
2019-05-23 16:06:01 +0200

    local db, without security (not all security occurrences were removed)

--- src/main/java/com/lending/SecurityConfig.java
+++ src/main/java/com/lending/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.lending;
-
+/*
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -49,3 +49,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
+*/
