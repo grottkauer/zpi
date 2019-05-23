18ebf997ba082bdb5b0b6dea087933ed7a3efd37
Natalia <natalia-gasiorowska@o2.pl>
2019-05-23 20:21:54 +0200

    changed category name string on product details

--- src/main/java/com/lending/repositories/ResourceRepository.java
+++ src/main/java/com/lending/repositories/ResourceRepository.java
@@ -57,12 +57,19 @@ public interface ResourceRepository extends CrudRepository<Resource, Integer> {
     @Query("select max(rr.borrowDate) from ResourceRenting rr where rr.resource=:id")
     Date getLatestBorrowDate(@Param("id") int id);
 
-    @Query("select new com.lending.dto.ResourceDetailsDto (r.id, r.name, r.points, rt.name,  \n" +
+    @Query("select new com.lending.dto.ResourceDetailsDto (r.id, r.name, r.points, \n" +
+           "CONCAT(case when hl3.name is not null then concat(hl3.name,' -  ') else '' end,\n" +
+            "case when hl2.name is not null then concat(hl2.name,' - ') else '' end, \n" +
+            "case when hl1.name is not null then concat(hl1.name,' - ') else '' end, \n" +
+            "rt.name), \n" +
             "(select rr2.status from ResourceRenting rr2 where rr2.orderDate = \n" +
             "(select max(rr3.orderDate) from ResourceRenting rr3 where rr3.resource = r.id) \n" +
             "and rr2.resource = r.id), r.description, (select count(rr.id) from ResourceRenting rr where rr.resource = r.id)) \n" +
             "from Resource r \n" +
             "inner join r.resourceType rt \n" +
+            "left join rt.higherLevel hl1 \n" +
+            "left join hl1.higherLevel hl2 \n" +
+            "left join hl2.higherLevel hl3 \n" +
             "where r.id=:id \n")
     ResourceDetailsDto getProductDetails(@Param("id") int id);
 
