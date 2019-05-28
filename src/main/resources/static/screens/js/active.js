1d66947c8d67faceaad5d285c6ba1e5ba9be2454
grottkauer <jaszyr@interia.pl>
2019-05-28 22:22:02 +0200

    fixed main carousel in media

--- src/main/resources/static/screens/js/active.js
+++ src/main/resources/static/screens/js/active.js
@@ -4,15 +4,30 @@ function shiftLeft() {
     boxes[0].className = "box move-out-from-left";
 
     setTimeout(function() {
-        if (boxes.length > 5) {
-            tmpNode.classList.add("box--hide");
-            boxes[5].className = "box move-to-position5-from-left";
+        if (window.matchMedia('(max-width: 767px)').matches) {
+            if (boxes.length > 3) {
+                tmpNode.classList.add("box--hide");
+                tmpNode.classList.add("media--hide");
+                boxes[3].className = "box move-to-position3-from-left";
+
+            }
+             boxes[1].className = "box move-to-position1-from-left";
+             boxes[2].className = "box move-to-position2-from-left";
+             boxes[0].remove();
         }
-        boxes[1].className = "box move-to-position1-from-left";
-        boxes[2].className = "box move-to-position2-from-left";
-        boxes[3].className = "box move-to-position3-from-left";
-        boxes[4].className = "box move-to-position4-from-left";
-        boxes[0].remove();
+        else{
+            if (boxes.length > 5) {
+                        tmpNode.classList.add("box--hide");
+                        boxes[5].className = "box move-to-position5-from-left";
+            }
+            boxes[1].className = "box move-to-position1-from-left";
+            boxes[2].className = "box move-to-position2-from-left";
+            boxes[3].className = "box move-to-position3-from-left";
+            boxes[4].className = "box move-to-position4-from-left";
+            boxes[0].remove();
+        }
+
+
 
         document.querySelector(".cards__container").appendChild(tmpNode);
 
